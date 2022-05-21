af673603fa9219c642a0980c6fc6ce002fc16fa2
DamirGuzaerov <86714479+DamirGuzaerov@users.noreply.github.com>
2022-05-21 20:37:33 +0300

    update procfile

---
+++ MyBook.Infrastructure/Migrations/20220521172414_newTable.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MyBook.Infrastructure.Migrations
+{
+    public partial class newTable : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
